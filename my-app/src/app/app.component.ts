693e6ee3c9c35ec13e2941de2f20ec3db6f227fb
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2022-06-16 12:45:08 +0200

    añadiendo estilos

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -14,6 +14,9 @@ export class AppComponent {
 
     {texto:"El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"}, 
     {texto:"Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes."}, 
+    {texto:"L'heroi va decidir travessar la porta que el portava a casa"
+    },
+
     {texto:"Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."} 
       
       
