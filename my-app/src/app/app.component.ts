a6f92f4efeba146298c40af98b902e9aa3b696e6
brokestar7 <82914463+brokestar7@users.noreply.github.com>
2022-06-21 11:43:54 +0200

    update

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -21,10 +21,6 @@ export class AppComponent {
       
       
   ]
-
-
-
-
     
     
   
--- my-app/src/app/escena/escena.component.ts
+++ my-app/src/app/escena/escena.component.ts
@@ -1,3 +1,4 @@
+import { CursorError } from '@angular/compiler/src/ml_parser/lexer';
 import { Component, OnInit, Input } from '@angular/core';
 import { cuento } from '../interface';
 
@@ -9,6 +10,32 @@ import { cuento } from '../interface';
 export class EscenaComponent implements OnInit {
 
   // @Input() textosEs:any;
+
+  currentsentence:number = 0;
+  id:any = 0;
+
+  prev(){
+    if(this.currentsentence>0){
+      this.currentsentence--
+      this.id--
+    }
+    else{
+      this.currentsentence = 3;
+      this.id=3
+    }
+  }
+
+  next(){
+    if(this.currentsentence>3){
+      this.currentsentence = 0;
+      this.id=0;
+    }
+    else{
+      this.currentsentence++;
+      this.id++
+    }
+  }
+
 
   @Input()  public textos :cuento[] = [{texto:""}] ;
 
